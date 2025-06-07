bd87d8c718ac64c5bf92940ddd43be9b05cf603a
unknown <phuandongthap02@gmail.com>
2025-06-07 09:57:21 +0700

    Github Actions: change ami and region

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  ami           = "ami-002e99de5ef075652" # Amazon Linux 2 (us-east-1)
+  ami           = "ami-003ce501eabea4d72"
   instance_type = "t2.micro"
   subnet_id     = var.subnet_id
   vpc_security_group_ids = [var.sg_id]
