8efb7949fa30cec2162881d74b789827d0a5182a
unknown <phuandongthap02@gmail.com>
2025-06-07 09:28:35 +0700

    GITHUB ACTIONS: add region, add ami, config aws credentials in github repo

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
 module "vpc" {
   source = "./modules/vpc"
   region = var.region
--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,8 +1,16 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 (us-east-1)
+  ami           = "ami-002e99de5ef075652" # Amazon Linux 2 (us-east-1)
   instance_type = "t2.micro"
   subnet_id     = var.subnet_id
   vpc_security_group_ids = [var.sg_id]
+  metadata_options {
+    http_tokens = "required"
+    http_endpoint = "enabled"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
 
   tags = {
     Name = "Terraform-EC2"
